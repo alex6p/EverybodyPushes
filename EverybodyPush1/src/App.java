e6ec5c89dc00055d000b11f3de764c41adc1b537
Alex Psitos <ajp5922@psu.edu>
2018-04-12 13:47:05 -0400

    creating project/ added Hello world

--- EverybodyPush1/src/App.java
+++ EverybodyPush1/src/App.java
@@ -6,7 +6,7 @@
 
 /**
  *
- * @author angie
+ * @author alexpsitos
  */
 public class App {
 
@@ -14,7 +14,7 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
